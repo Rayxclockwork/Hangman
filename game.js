175bb7fbd382c90218c1bd0f607d88f42791a8b3
RAVEN DELANEY <ravend17@gmail.com>
2021-01-13 18:18:23 -0600

    game function

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
+function game () {
+	this.words = ["computer", "bed", "shelf", "desk"];
+	this.chosenword = "";
+	this.guessed = [];
+	this.possible = 
+	this.lives = 6;
 
+	this.chosenword = this.getword();
+}
+
+game.prototype.getword = function(){
+	let random = Math.floor(Math.random() * this.words.length);
+	return this.words[random];
+}
+
+const game = require("./start.js");
+const end = require("./end.js");
